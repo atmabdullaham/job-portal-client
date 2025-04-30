e96327f6c7725f0cf72b692d24ed99e94456fc6e
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2025-04-30 22:22:52 +0600

    added react router and registration page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,31 +1,109 @@
-import React from 'react';
+import Lottie from "lottie-react";
+import React from "react";
+import ReactDOM from "react-dom";
+import { useForm } from "react-hook-form";
+import registerAnimation from "../assets/register.json";
+import regAnimation from "../assets/reg.json";
 
 const Register = () => {
-    return (
-        <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Register</h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-    </div>
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <div className="card-body">
-        <fieldset className="fieldset">
-          <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
-          <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
-          <div><a className="link link-hover">Forgot password?</a></div>
-          <button className="btn btn-neutral mt-4">Login</button>
-        </fieldset>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {},
+  });
+  const onSubmit = (data) => {
+    console.log(data.email, data.password, data.displayName);
+  };
+
+  return (
+    <div className="hero bg-base-200 min-h-[calc(100vh-280px)]">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="hidden  lg:block text-center lg:text-left">
+          <Lottie
+            className="h-[500px]"
+            animationData={registerAnimation}
+          ></Lottie>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <Lottie
+            className="h-30 w-20 mx-auto"
+            animationData={regAnimation}
+          ></Lottie>
+          <div className="card-body text-black">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4 pt-0"
+            >
+              <legend className="fieldset-legend text-xl mx-auto font-bold">
+                Register
+              </legend>
+              {/* email */}
+              <label className="label">Email</label>
+              <input
+                {...register("email", {
+                  required: true,
+                })}
+                className="input"
+                placeholder="Email"
+                type="email"
+              />
+              {errors.email && (
+                <p className="text-red-400">This field is required</p>
+              )}
+              {/* display name */}
+              <label className="label">Display Name</label>
+              <input
+                {...register("displayName", {
+                  required: true,
+                })}
+                type="text"
+                className="input"
+                placeholder="write your name"
+                maxLength="30"
+                minLength="3"
+              />
+              {errors.displayName && (
+                <p className="text-red-400">This field is required</p>
+              )}
+
+              {/* Password */}
+              <label className="label">Password</label>
+              <input
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 8,
+                    message: "Password must be at least 8 characters",
+                  },
+                  maxLength: {
+                    value: 10,
+                    message: "Password must be at most 10 characters",
+                  },
+                  pattern: {
+                    value: /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                    message:
+                      "Must contain at least one uppercase letter and one number",
+                  },
+                })}
+                type="password"
+                className="input"
+                placeholder="Password"
+              />
+
+              {/* Show specific validation messages */}
+              {errors.password && (
+                <p className="text-red-400">{errors.password.message}</p>
+              )}
+
+              <input className="btn btn-neutral mt-4" type="submit" />
+            </form>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default Register;
